78c4b41320a8cd74cc66571d97f179ba1992b319
zhaochao <zhaochao@rd.netease.com>
2018-11-12 10:37:23 +0800

    OcTest

---
+++ OcTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OcTest
+//
+//  Created by 赵超 on 2018/11/12.
+//  Copyright © 2018 赵超. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"this is my first ocTest");
+    }
+    return 0;
+}
