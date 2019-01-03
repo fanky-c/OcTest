1549a70c84b3d33b9c8755021a6da104a712c0d2
zhaochao <zhaochao@rd.netease.com>
2019-01-03 19:34:31 +0800

    update

--- UIButton/UIButton/ViewController.m
+++ UIButton/UIButton/ViewController.m
@@ -14,9 +14,60 @@
 
 @implementation ViewController
 
+
+- (void) createRectButton{
+    //只能通过类方法创建，不能通过实例方法。uibutton自己管理自己的内存
+    UIButton * Btn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    
+    //设置位置
+    Btn.frame = CGRectMake(100, 100, 100, 40);
+    
+    
+    //按钮背景色
+    Btn.backgroundColor = [UIColor grayColor];
+    
+    
+    //设置字体大小
+    Btn.titleLabel.font = [UIFont systemFontOfSize:16];
+    
+    //设置按钮状态和内容
+    //默认状态
+    [Btn setTitle:@"按钮" forState:UIControlStateNormal];
+    [Btn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+    
+    //按下状态
+    [Btn setTitle:@"按下" forState:UIControlStateHighlighted];
+    [Btn setTitleColor:[UIColor blueColor] forState:UIControlStateHighlighted];
+
+    
+    //显示按钮
+    [self.view addSubview:Btn];
+    
+    
+}
+
+
+-(void) createImageBtn{
+    UIButton * Btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    
+    Btn.frame = CGRectMake(100, 200, 100, 40);
+    
+    UIImage * icon1 = [UIImage imageNamed:@"ic_scene.png"];
+    UIImage * icon2 = [UIImage imageNamed:@"ic_screen.png"];
+    
+    [Btn setImage:icon1 forState:UIControlStateNormal];
+    [Btn setImage:icon2 forState:UIControlStateHighlighted];
+    
+    [self.view addSubview:Btn];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self createRectButton];
+    
+    [self createImageBtn];
 }
 
 
