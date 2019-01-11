89a947f10d8bc63466dd2493753c016c8a82a660
zhaochao <zhaochao@rd.netease.com>
2019-01-11 10:26:42 +0800

    add

---
+++ 手动布局/手动布局/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  手动布局
+//
+//  Created by 赵超 on 2019/1/10.
+//  Copyright © 2019 赵超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
