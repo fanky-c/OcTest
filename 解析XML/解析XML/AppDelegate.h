40bc2f503a7efbc16a89f6568d6d7b584ba427c0
zhaochao <zhaochao@rd.netease.com>
2019-02-11 17:10:34 +0800

    add:xml

---
+++ 解析XML/解析XML/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  解析XML
+//
+//  Created by 赵超 on 2019/2/10.
+//  Copyright © 2019 赵超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 解析XML/解析XML/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  解析XML
+//
+//  Created by 赵超 on 2019/2/10.
+//  Copyright © 2019 赵超. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
